144cbb16a778e8198ddfa15a32fb841efc2fbbc3
crazymalady <jennifer.lacaste2455@gmail.com>
2021-05-29 07:00:52 +0800

    output-dicee-game

---
+++ dicee-flutter/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          centerTitle: true,
+          textTheme: TextTheme(
+            headline6: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  // variables
+  int leftDiceNum = 1; //put here outside build to prevent it to always building it everytime. So it will only work on first build.
+  int rightDiceNum = 1;
+
+  // functions
+  void changeDiceFace(){
+    setState(() {
+      leftDiceNum = Random().nextInt(6) + 1;
+      rightDiceNum = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNum.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$rightDiceNum.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
